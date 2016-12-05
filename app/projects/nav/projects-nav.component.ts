1e64ea04960f7e6308e2f135a005147dd9ea0163
Brooks Becton <broabect@ut.utm.edu>
2016-12-04 19:44:53 -0600

    Added some pedantic css

--- app/projects/nav/projects-nav.component.ts
+++ app/projects/nav/projects-nav.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core'
 @Component({
     moduleId: module.id,
     selector: 'projects-nav',
+    styleUrls: ['projects-nav.component.css'],
     templateUrl: 'projects-nav.component.html',
 })
 
-export class ProjectNavComponent{
+export class ProjectNavComponent {
 
 }
